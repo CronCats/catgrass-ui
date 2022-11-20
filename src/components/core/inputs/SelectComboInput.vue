5a197e3808bdc9c43b79e19e050eae90b1c13d6d
TrevorJTClarke <trevorjtclarke@gmail.com>
2022-11-20 11:45:42 -0800

    migration input

--- src/components/core/inputs/SelectComboInput.vue
+++ src/components/core/inputs/SelectComboInput.vue
@@ -1,9 +1,38 @@
-import clsx from 'clsx'
-import { ChangeEvent, useEffect, useState } from 'react'
-import { FieldPathValue, FieldValues, Path, Validate } from 'react-hook-form'
+<template>
+  <div
+    :class="{
+      'flex w-full text-lg text-right bg-white rounded-lg border-2 focus-within:outline-none focus-within:ring-2 ring-offset-0 transition border-default': true,
+      'ring-2 ring-red-700 shadow-md shadow-red-400': error,
+      'ring-transparent': !error,
+      ...containerClassName
+    }"
+  >
+    <input
+      :class="{
+        'py-[14px] px-3 w-2/3 text-lg bg-transparent border-none outline-none ring-none': true,
+        ...className
+      }"
+      name="input"
+      @change="updateInput"
+      type="text"
+      v-model="input"
+    />
+    <select
+      :class="{
+        'py-[14px] px-3 w-1/3 text-body bg-transparent border-l-2 focus:outline-none focus:ring-none border-default': true
+      }"
+      name="select"
+      @change="updateSelect"
+      v-model="select"
+    >
+      <option v-for="(option, index) in options" :key="index" :value="option.value">
+        {{option.key}}
+      </option>
+    </select>
+  </div>
+</template>
 
-import { usePrevious } from '@croncat-ui/utils'
-
+<script lang="ts">
 export interface ComboInputSelectValue {
   select: string
   input: string
@@ -15,95 +44,35 @@ export interface ComboInputSelectOption {
   default: string
 }
 
-export interface ComboInputSelectProps<
-  FV extends FieldValues,
-  FieldName extends Path<FV>
-> {
-  options: ComboInputSelectOption[]
-  onChange: (data: ComboInputSelectValue) => void
-  containerClassName?: string
-  className?: string
-  error?: boolean
-  validation?: Validate<FieldPathValue<FV, FieldName>>[]
-}
+export default {
+  props: ["options", "onChange", "containerClassName", "className", "error", "validation"],
 
-export const ComboInputSelect = <
-  FV extends FieldValues,
-  FieldName extends Path<FV>
->({
-  options,
-  onChange,
-  containerClassName,
-  className,
-  error,
-  validation,
-  ...props
-}: ComboInputSelectProps<FV, FieldName>) => {
-  const [state, setState] = useState(() => {
-    const first = options[0]
-    return { input: first.default, select: first.value }
-  })
-
-  // TOOD: Finish!
-  const validate = validation?.reduce(
-    (a, v) => ({ ...a, [v.toString()]: v }),
-    {}
-  )
-
-  const lastState = usePrevious(state)
-
-  const updateInput = (e: ChangeEvent<HTMLInputElement>) => {
-    setState({ ...state, input: e.target.value })
-  }
-  const updateSelect = (e: ChangeEvent<HTMLSelectElement>) => {
-    setState({ ...state, select: e.target.value })
-  }
-
-  useEffect(() => {
-    if (lastState?.select !== state.select) {
-      const index = options.findIndex((v) => v.value === state.select)
-      setState({ ...state, input: options[index].default })
+  data() {
+    return {
+      input: '',
+      prevInput: '',
+      select: this.options[0] || {},
+      prevSelect: this.options[0] || {},
     }
-    onChange(state)
-  }, [state])
+  },
 
-  return (
-    <div
-      className={clsx(
-        'flex w-full text-lg text-right bg-white rounded-lg border-2 focus-within:outline-none focus-within:ring-2 ring-offset-0 transition border-default',
-        {
-          'ring-2 ring-red-700 shadow-md shadow-red-400': error,
-          'ring-transparent': !error,
-        },
-        containerClassName
-      )}
-    >
-      <input
-        className={clsx(
-          'py-[14px] px-3 w-2/3 text-lg bg-transparent border-none outline-none ring-none',
-          className
-        )}
-        name="input"
-        onChange={updateInput}
-        type="text"
-        value={state.input}
-        {...props}
-      />
-      <select
-        className={clsx(
-          'py-[14px] px-3 w-1/3 text-body bg-transparent border-l-2 focus:outline-none focus:ring-none border-default'
-        )}
-        name="select"
-        onChange={updateSelect}
-        value={state.select}
-        {...props}
-      >
-        {options.map((option, index) => (
-          <option key={index} value={option.value}>
-            {option.key}
-          </option>
-        ))}
-      </select>
-    </div>
-  )
-}
+  methods: {
+    updateInput() {
+      if (this.prevInput === this.input) return
+      this.prevInput = this.input
+      this.onChange({
+        input: this.input,
+        select: this.select,
+      })
+    },
+    updateSelect() {
+      if (this.prevSelect === this.select) return
+      this.prevSelect = this.select
+      this.onChange({
+        input: this.input,
+        select: this.select,
+      })
+    },
+  },
+};
+</script>
--- src/components/core/inputs/SelectList.vue
+++ src/components/core/inputs/SelectList.vue
@@ -1,120 +1,75 @@
-import { Listbox, Transition } from '@headlessui/react'
-import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid'
-import clsx from 'clsx'
-import { ComponentType, Fragment, useState } from 'react'
-
-export interface SelectListValue {
-  title: string
-  data: any
-  rules?: any[]
-  Icon?: ComponentType
-  sort?: number
-}
-
-export interface SelectListOption {
-  key: string
-  value: SelectListValue
-}
-
-export interface SelectListProps {
-  options: SelectListOption[]
-  onChange: (value: any) => void
-  containerClassName?: string
-  className?: string
-  name?: string
-}
-
-export const SelectList = ({
-  options,
-  onChange,
-  containerClassName,
-  className,
-  ...props
-}: SelectListProps) => {
-  const [state, setState] = useState(options[0])
-
-  const updateSelect = (item: any) => {
-    setState({ ...item })
-    onChange(item)
-  }
-
-  return (
-    <Listbox {...props} onChange={updateSelect} value={state}>
-      {({ open }) => (
-        <>
-          <div className="relative mt-1">
-            <Listbox.Button className="relative py-3 pr-10 pl-3 w-full text-left bg-white rounded-md border-2 border-gray-200 focus:border-gray-200 focus:outline-none focus:ring-0 focus:ring-gray-200 cursor-default sm:text-sm">
-              <span className="flex items-center">
-                {/* <selected.icon className="h-6 w-6 flex-shrink-0" /> */}
-                {state.value.Icon ? (
-                  <div className="flex-shrink-0 w-8 h-8">
-                    <state.value.Icon />
-                  </div>
-                ) : (
-                  ''
-                )}
-                <span className="block ml-3 text-lg font-bold truncate">
-                  {state.value.title}
-                </span>
-              </span>
-              <span className="flex absolute inset-y-0 right-0 items-center mr-3 pointer-events-none">
-                <ChevronUpDownIcon aria-hidden="true" className="w-8 h-7" />
-              </span>
-            </Listbox.Button>
-
-            <Transition
-              as={Fragment}
-              leave="transition ease-in duration-100"
-              leaveFrom="opacity-100"
-              leaveTo="opacity-0"
-              show={open}
-            >
-              <Listbox.Options className="overflow-auto absolute z-10 py-1 mt-0 w-full max-h-56 text-base bg-white rounded-md focus:outline-none ring-2 ring-gray-200 shadow-lg sm:text-sm">
-                {options.map((item, index) => (
-                  <Listbox.Option
-                    key={index}
-                    className={({ active }) =>
-                      clsx(
-                        active ? 'text-gray-800 bg-gray-200' : 'text-gray-900',
-                        'relative py-2 pr-9 pl-3 cursor-default select-none'
-                      )
-                    }
-                    value={item}
-                  >
-                    {({ selected, active }) => (
-                      <>
-                        <div className="flex items-center">
-                          {item.value.Icon ? (
-                            <div className="flex-shrink-0 w-8 h-8">
-                              <item.value.Icon />
-                            </div>
-                          ) : (
-                            ''
-                          )}
-                          <span className="block ml-3 text-lg font-bold truncate">
-                            {item.value.title}
-                          </span>
-                        </div>
-
-                        {selected ? (
-                          <span
-                            className={clsx(
-                              active ? 'text-gray-800' : 'text-gray-600',
-                              'flex absolute inset-y-0 right-0 items-center pr-4'
-                            )}
-                          >
-                            <CheckIcon aria-hidden="true" className="w-5 h-5" />
-                          </span>
-                        ) : null}
-                      </>
-                    )}
-                  </Listbox.Option>
-                ))}
-              </Listbox.Options>
-            </Transition>
+<template>
+  <Listbox :onChange="updateSelect"  v-model="selected">
+    <div class="relative mt-1">
+      <ListboxButton class="relative py-3 pr-10 pl-3 w-full text-left bg-white rounded-md border-2 border-gray-200 focus:border-gray-200 focus:outline-none focus:ring-0 focus:ring-gray-200 cursor-default sm:text-sm">
+        <span class="flex items-center">
+          <div v-if="selected.Icon" class="flex-shrink-0 w-8 h-8">
+            <component :is="selected.Icon" />
           </div>
-        </>
-      )}
-    </Listbox>
-  )
-}
+          <span class="block ml-3 text-lg font-bold truncate">
+            {{selected.title}}
+          </span>
+        </span>
+        <span class="flex absolute inset-y-0 right-0 items-center mr-3 pointer-events-none">
+          <ChevronUpDownIcon aria-hidden="true" class="w-8 h-7" />
+        </span>
+      </ListboxButton>
+    
+      <transition leave-active-class="transition duration-100 ease-in" leave-from-class="opacity-100" leave-to-class="opacity-0">
+        <ListboxOptions
+          class="overflow-auto absolute z-10 py-1 mt-0 w-full max-h-56 text-base bg-white rounded-md focus:outline-none ring-2 ring-gray-200 shadow-lg sm:text-sm">
+          <ListboxOptions v-for="(item, index) in options" v-slot="{ active, selected }" :key="index" :class="[
+            // active ? 'text-gray-800 bg-gray-200' : 'text-gray-900',
+            'relative py-2 pr-9 pl-3 cursor-default select-none',
+            ]"
+            :value="item"
+          >
+            <div class="flex items-center">
+              <div v-if="item.value.Icon" class="flex-shrink-0 w-8 h-8">
+                <component :is="item.value.Icon" />
+              </div>
+              <span class="block ml-3 text-lg font-bold truncate">
+                {{item.value.title}}
+              </span>
+            </div>
+
+            <span v-if="selected" :class="[ active ? 'text-gray-800' : 'text-gray-600' , 'flex absolute inset-y-0 right-0 items-center pr-4' ]">
+              <CheckIcon aria-hidden="true" class="w-5 h-5" />
+            </span>
+          </ListboxOptions>
+        </ListboxOptions>
+      </transition>
+    </div>
+  </Listbox>
+</template>
+
+<script lang="ts">
+import {
+  Listbox,
+  ListboxLabel,
+  ListboxButton,
+  ListboxOptions,
+  ListboxOption,
+} from '@headlessui/vue'
+import { CheckIcon, ChevronUpDownIcon } from '@heroicons/vue/20/solid'
+
+export default {
+  props: ["options", "updateSelect", "containerClassName", "className"],
+
+  components: {
+    CheckIcon,
+    ChevronUpDownIcon,
+    Listbox,
+    ListboxLabel,
+    ListboxButton,
+    ListboxOptions,
+    ListboxOption,
+  },
+
+  data() {
+    return {
+      selected: this.options[0] || {}
+    }
+  },
+};
+</script>
